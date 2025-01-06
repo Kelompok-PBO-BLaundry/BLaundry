7a9a896005932dc0bf4604b19f07dff094e04580
nexocarryus <naufal.dzakwann28@gmail.com>
2025-01-07 03:26:06 +0700

    Customer.java

---
+++ src/main/java/model/Customer.java
@@ -0,0 +1,31 @@
+package model;
+
+public class Customer extends User {
+    private String phoneNumber;
+    private String address;
+
+    public Customer() {
+    }
+
+    public Customer(String username, String password, String phoneNumber, String address) {
+        super(username, password);
+        this.phoneNumber = phoneNumber;
+        this.address = address;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}
