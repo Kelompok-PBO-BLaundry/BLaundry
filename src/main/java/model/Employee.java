49a83ec55680ae81e0504fe9d342a28a3d377bf7
Faqih Altaf Ghifari <faqih.altaf4@gmail.com>
2025-01-07 04:48:00 +0700

    Employee.java

---
+++ src/main/java/model/Employee.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+public class Employee extends User{
+    public Employee() {
+    }
+
+    public Employee(String username, String password) {
+        super(username, password);
+    }
+}
